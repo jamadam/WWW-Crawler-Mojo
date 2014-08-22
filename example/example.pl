5f6e41a356782fd5fffa683b1ba9676c64248b7f
jamadam <sugama@jamadam.com>
2014-08-22 23:26:36 +0900

    updated example

--- example/example.pl
+++ example/example.pl
@@ -2,21 +2,20 @@ use strict;
 use warnings;
 use utf8;
 use Mojo::Crawler;
+use 5.10.0;
 
 my $bot = Mojo::Crawler->new(
     on_res => sub {
-        my ($crawl, $queue, $tx) = @_;
-        $crawl->();
+        my ($discover, $queue, $tx) = @_;
+        say sprintf('fetching %s resulted status %s',
+                                        $queue->resolved_uri, $tx->res->code);
+        $discover->();
     },
     on_refer => sub {
-        my ($append, $queue, $parent_queue, $context) = @_;
-        $append->();
+        my ($enqueue, $queue, $parent_queue, $context) = @_;
+        $enqueue->();
     },
-    after_crawl => sub {
-        my ($queue, $tx) = @_;
-    }
 );
 
-$bot->append_queue('http://example.com/');
-$bot->depth(2);
+$bot->enqueue('http://example.com/');
 $bot->crawl;
