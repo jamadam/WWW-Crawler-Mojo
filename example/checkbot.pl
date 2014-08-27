37fb9922697339c533c486ce500c696e3b846e8a
jamadam <sugama@jamadam.com>
2014-08-27 17:40:23 +0900

    made checkbot example more practical

--- example/checkbot.pl
+++ example/checkbot.pl
@@ -5,23 +5,33 @@ use Mojo::Crawler;
 use 5.10.0;
 
 my $bot = Mojo::Crawler->new;
+my %count;
+
 $bot->on(res => sub {
     my ($bot, $discover, $queue, $res) = @_;
+    
+    $count{$res->code}++;
+    
     if ($res->code == 404) {
         say sprintf('404 occured! : %s referred by %s',
-                                    $queue->resolved_uri);
-    } else {
-        say sprintf('fetching %s resulted status %s',
-                                            $queue->resolved_uri, $res->code);
+                                    $queue->resolved_uri, $queue->referrer);
     }
+    
+    my @disp_seed;
+    push(@disp_seed, sprintf('%s:%s', $_, $count{$_})) for (keys %count);
+    
+    $| = 1;
+    print(join(' / ', @disp_seed), ' ' x 30);
+    print("\r");
     
     $discover->();
 });
+
 $bot->on(refer => sub {
     my ($bot, $enqueue, $queue, $parent_queue, $context) = @_;
     $enqueue->();
 });
 
-$bot->enqueue('http://google.com/');
+$bot->enqueue('http://example.com/');
 $bot->peeping_port(3001);
 $bot->crawl;
