94e99f3194cc4866e35ef6187288841762e4b962
jamadam <sugama@jamadam.com>
2014-09-07 01:16:10 +0900

    bump version

--- lib/WWW/Crawler/Mojo.pm
+++ lib/WWW/Crawler/Mojo.pm
@@ -8,7 +8,7 @@ use WWW::Crawler::Mojo::UserAgent;
 use Mojo::Message::Request;
 use Mojo::Util qw{md5_sum xml_escape dumper};
 use List::Util;
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 has active_conn => 0;
 has 'crawler_loop_id';
