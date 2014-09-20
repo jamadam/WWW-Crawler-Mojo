126f30daea95cec01fa9d5fe062ca15739367a70
jamadam <sugama@jamadam.com>
2014-09-20 12:48:14 +0900

    improved checkbot example

--- example/checkbot.pl
+++ example/checkbot.pl
@@ -14,6 +14,17 @@ $bot->on(start => sub {
     shift->say_start;
 });
 
+$bot->on(error => sub {
+    my ($bot, $msg, $job) = @_;
+    
+    $count{'N/A'}++;
+    
+    chomp($msg);
+    
+    say sprintf('%s : %s referred by %s',
+                        $msg, $job->resolved_uri, $job->referrer->resolved_uri);
+});
+
 $bot->on(res => sub {
     $| = 1;
     
@@ -29,7 +40,7 @@ $bot->on(res => sub {
     my @disp_seed;
     push(@disp_seed, sprintf('%s:%s', $_, $count{$_})) for (keys %count);
     
-    print(join(' / ', @disp_seed), ' ' x 30);
+    print(join(' / ', @disp_seed), ' / QUEUE:'. scalar @{$bot->queue}, ' ' x 30);
     print("\r");
     
     $discover->();
@@ -37,12 +48,33 @@ $bot->on(res => sub {
 
 $bot->on(refer => sub {
     my ($bot, $enqueue, $job, $context) = @_;
-    if ($job->referrer->resolved_uri->host eq $start->host) {
-        $enqueue->();
+    
+    if (security_warning($job, $context)) {
+        say sprintf('WARNING : Cross-scheme resource found at %s referred by %s',
+                            $job->resolved_uri, $job->referrer->resolved_uri);
     }
+    
+    $enqueue->() if ($job->referrer->resolved_uri->host eq $start->host);
 });
+
+$bot->shuffle(5);
 $bot->max_conn_per_host(2);
 $bot->max_conn(5);
 $bot->enqueue($start);
 $bot->peeping_port(3001);
 $bot->crawl;
+
+sub security_warning {
+    my ($job, $context) = @_;
+    
+    my $scheme1 = $job->referrer->resolved_uri->protocol;
+    my $scheme2 = $job->resolved_uri->protocol;
+    
+    if ($scheme1 eq 'https' && $scheme2 ne 'https') {
+        return 1 if (!ref $context || ref $context ne 'Mojo::DOM');
+        return 0 if ($context->type eq 'a');
+        return 1;
+    }
+    
+    return 0;
+}
